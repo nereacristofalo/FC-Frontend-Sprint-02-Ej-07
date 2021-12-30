86a9d6fe55e40ed3fa36dc64a67a3e89e650bf39
nereacristofalo <nmcristofalo@gmail.com>
2021-12-30 18:07:10 +0100

    ej 07

---
+++ src/components/bodyContainer/BodyContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './BodyContainer.css';
+import FormContainer from './formContainer/FormContainer';
+
+const BodyContainer = () => {
+  return (
+    <div className="content-container">
+      <FormContainer />
+    </div>
+  );
+};
+
+export default BodyContainer;
